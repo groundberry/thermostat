d9545f37bc64508680cce580bff36e49ad14a46d
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-26 16:44:14 +0100

    Can't decrease temperature below minimum

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -19,3 +19,11 @@ describe("Thermostat", function() {
     thermostat.down();
     expect(thermostat.temperature).toEqual(19);
   });
+
+  it("can't decrease temperature below 10 degrees", function(){
+    var i;
+    for (i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.temperature).toBeGreaterThan(9);
+  });
