7f4602fdcf7fe2f57331f50c8ae64e40161549f3
James Gardiner <jamesgardiner075@gmail.com>
2016-10-26 16:11:56 +0100

    Add 'up' function to increase temperature

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -2,10 +2,15 @@ describe("Thermostat", function() {
   var thermostat;
 });
 
-beforeEach(function() {
-  thermostat = new Thermostat();
-});
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
 
-it("has a starting temperature of 20 °C", function() {
-  expect(thermostat.temperature).toEqual(20);
-});
+  it("has a starting temperature of 20 °C", function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+
+  it("allows increase of temperature", function(){
+    thermostat.up();
+    expect(thermostat.temperature).toEqual(21);
+  });
