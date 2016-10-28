89c89b1544a82a7541aea196cd4f8333dd0df808
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-28 12:27:16 +0100

    Implement Power Saving Mode functionality using JQuery

--- interface.js
+++ interface.js
@@ -6,17 +6,26 @@ $(document).ready(function() {
     thermostat.up();
     document.getElementById('degrees').innerHTML=(thermostat.temperature);
   });
-  document.getElementById('degrees').innerHTML=(thermostat.temperature);
 
   $('#temperature-down').click(function() {
     thermostat.down();
     document.getElementById('degrees').innerHTML=(thermostat.temperature);
   });
-  document.getElementById('degrees').innerHTML=(thermostat.temperature);
 
   $('#temperature-reset').click(function() {
     thermostat.reset();
     document.getElementById('degrees').innerHTML=(thermostat.temperature);
   });
-  document.getElementById('degrees').innerHTML=(thermostat.temperature);
+
+  $('#power_saving-on').click(function() {
+    thermostat.powerSavingMode(true);
+      $('#power_saving-on').addClass('red').removeClass('black');
+      $('#power_saving-off').addClass('black').removeClass('red');
+  });
+
+  $('#power_saving-off').click(function() {
+    thermostat.powerSavingMode(false);
+    $('#power_saving-off').addClass('red').removeClass('black');
+    $('#power_saving-on').addClass('black').removeClass('red');
+  });
 });
