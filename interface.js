1b6b74565e12a48cc92b8b8d93cab5c39e963e44
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-28 14:40:02 +0100

    Add Energy Usage functionality using jQuery

--- interface.js
+++ interface.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
   $('#temperature-up').click(function() {
     thermostat.up();
     document.getElementById('degrees').innerHTML=(thermostat.temperature);
+    $('#temperature-up').usageLight();
   });
 
   $('#temperature-down').click(function() {
     thermostat.down();
     document.getElementById('degrees').innerHTML=(thermostat.temperature);
+    $('#temperature-up').usageLight();
   });
 
   $('#temperature-reset').click(function() {
@@ -28,4 +30,21 @@ $(document).ready(function() {
     $('#power_saving-off').addClass('red').removeClass('black');
     $('#power_saving-on').addClass('black').removeClass('red');
   });
+
+  (function( $ ) {
+    $.fn.usageLight = function() {
+      var usage = thermostat.energyUsage();
+      if (usage === 'low-usage') {
+        $('#energy-saving-low').addClass('red');
+        $('#energy-saving-medium').removeClass('red');
+      } else if (usage === 'medium-usage') {
+        $('#energy-saving-medium').addClass('red');
+        $('#energy-saving-low').removeClass('red');
+        $('#energy-saving-high').removeClass('red');
+      } else {
+        $('#energy-saving-high').addClass('red');
+        $('#energy-saving-medium').removeClass('red');
+      }
+    };
+  }) (jQuery);
 });
